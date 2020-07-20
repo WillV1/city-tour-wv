ff27527702482db845a23f50ccb54692cada617f
WillV1 <wmvanhook@gmail.com>
2020-07-20 12:01:38 -0400

    Updates

---
+++ precache-manifest.5d8f769ffc5f96bbf0adbcc220cd559f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08221773fb8f2bda74200542393f5d93",
+    "url": "/city-tour/index.html"
+  },
+  {
+    "revision": "02a6a50a10be229ebf94",
+    "url": "/city-tour/static/css/main.88579e12.chunk.css"
+  },
+  {
+    "revision": "07f963ec9f1d55c01c53",
+    "url": "/city-tour/static/js/2.84ef5f7a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/city-tour/static/js/2.84ef5f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a6a50a10be229ebf94",
+    "url": "/city-tour/static/js/main.2c5db87c.chunk.js"
+  },
+  {
+    "revision": "4cfa484106e750360a2d",
+    "url": "/city-tour/static/js/runtime-main.1333d393.js"
+  },
+  {
+    "revision": "12a6659ff5467c71b599841286fb8e7f",
+    "url": "/city-tour/static/media/logo.12a6659f.svg"
+  }
+]);
