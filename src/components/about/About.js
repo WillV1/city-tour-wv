9e24bf2cb09f46d5d8d91812a5f91c364dc0a2bc
WillV1 <wmvanhook@gmail.com>
2020-07-20 21:45:49 -0400

    Update home and about pages

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import './About.scss';
+import Card from 'react-bootstrap/Card'
 
 const About = () => {
     return (
-        <div>About</div>
+            <Card className="about-card">
+                <Card.Body className ="about-page">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie, massa euismod congue posuere, 
+                augue mi dignissim nisl, at porttitor neque nisl id lectus. Vestibulum ante ipsum primis in faucibus orci luctus 
+                et ultrices posuere cubilia curae; In sed dolor eget elit lobortis maximus nec ut risus. Nam vitae leo dignissim, 
+                hendrerit nunc non, finibus erat. Curabitur nec ipsum sed sem tempor accumsan venenatis eu ligula. 
+                Suspendisse vulputate ipsum quis ipsum cursus gravida. 
+            
+                Vivamus fringilla lacus tristique, placerat enim at, 
+                auctor risus. Maecenas vel risus vitae quam dapibus placerat. Vestibulum scelerisque volutpat nulla, eget 
+                sodales arcu viverra quis. Vivamus ac magna sit amet lorem iaculis imperdiet blandit et dui. Curabitur 
+                pellentesque nec sapien feugiat feugiat. Proin dictum elementum sagittis. 
+                Vivamus lacus urna, accumsan nec justo nec, congue consequat lacus.</p>
+                </Card.Body>
+            </Card>
     )
 }
 
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import './Home.scss';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+
+import woman from '../img/woman.jpg';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <Container fluid={true}>
+            <div className="card-deck">
+                <Card className="home-card">
+                    <Card.Img className="home-image" variant="top" src={woman} alt="woman with map" />
+                </Card>
+                <Card className="home-card text">
+                    <Card.Body className="home-text">
+                        <h5>Welcome to City Tours, your one-stop location to learn about a variety of fun tours in fun locations.
+                        Check out our pages to learn more about us and the tours we have to offer!
+                        </h5>
+                    </Card.Body>
+                </Card>
+            </div>
+        </Container>
     )
 }
 
